1d5eeee822f6972f2ab29c8d276045e3f83796c5
Andrei_Hurynovich <twain575@gmail.com>
2024-03-22 15:55:53 +0300

    add desctop version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log('---- main js loaded');
+
+const value = document.querySelector("#form__value-output");
+const input = document.querySelector("#form__volume");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+  value.textContent = event.target.value;
+});
+
+document.querySelector('.input-file input[type=file]').addEventListener('change', function() {
+  let file = this.files[0];
+  this.nextElementSibling.innerHTML = file.name;
+});
